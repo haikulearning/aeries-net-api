e4ed9513859b844a53f5d4d2be5870377b2140aa
tgarrido-avantica <tomas.garrido@avantica.net>
2015-06-26 13:05:11 -0700

    Removes / at end of Aeries url

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,5 @@ RSpec.configure do |c|
   c.add_setting :aeries_certificate
   c.aeries_certificate='477abe9e7d27439681d62f4e0de1f5e1'
   c.add_setting :aeries_url
-  c.aeries_url='https://demo.aeries.net/aeries.net/'
+  c.aeries_url='https://demo.aeries.net/aeries.net'
 end
