4761fcb4d116e1138464346826c7d3eb01474ef6
tgarrido-avantica <tomas.garrido@avantica.net>
2015-06-26 13:13:38 -0700

    Adds 'inspect' and 'attributes' methods to Aeries objects.

--- lib/aeries_net_api/models/school.rb
+++ lib/aeries_net_api/models/school.rb
@@ -4,22 +4,31 @@
 module AeriesNetApi
   module Models
     class School < AeriesNetApi::Models::BaseModel
-#ToDo: make sure that this code executes just once.  Looks only runs when class is first instantiated
-      @@aeries_attributes_list= %w{Name InactiveStatusCode Address AddressCity AddressState AddressZipCode AddressZipExt DoNotReport StateCountyID StateDistrictID
+#ToDo: make sure that this code executes just once.  Looks like only runs when class is first instantiated
+      @@aeries_attributes_list= %w{SchoolCode Name InactiveStatusCode Address AddressCity AddressState AddressZipCode AddressZipExt DoNotReport StateCountyID StateDistrictID
            StateSchoolID LowGradeLevel HighGradeLevel Terms}
 
-      @@attributes_list = @@aeries_attributes_list.map { |attribute| "#{self.underscore(attribute)}=".to_sym }
+      @@setters_list = @@aeries_attributes_list.map { |attribute| "#{self.underscore(attribute)}=".to_sym }
 
-      @@attributes_list.each do |attribute|
-        # removes = from attribute name
-        attr_accessor attribute[0..attribute.length-2]
+      @@setters_list.each do |attribute|
+        # removes '=' from attribute name
+        attr_accessor attribute[0..-2]
       end
 
       puts "executing ****"
 # ToDo: End of code to test for running once.
 
       def initialize(aeries_data=nil)
-        parse(aeries_data, @@aeries_attributes_list, @@attributes_list) if aeries_data.present?
+        parse(aeries_data, @@aeries_attributes_list, @@setters_list) if aeries_data.present?
+      end
+
+      # Overrides 'inspect' method to show only created attributes
+      def inspect
+        model_inspect(@@setters_list)
+      end
+
+      def attributes
+        model_attributes(@@setters_list)
       end
     end
   end
