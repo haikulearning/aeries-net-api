b49398adf831c4ba5e8615d929f980053533eb91
tgarrido-avantica <tomas.garrido@avantica.net>
2015-06-30 11:33:47 -0700

    Improves documentation and adds tests for School model

--- lib/aeries_net_api/models/school.rb
+++ lib/aeries_net_api/models/school.rb
@@ -1,18 +1,21 @@
-# ToDo: Create documentation
-# ToDo: Create tests
-
 module AeriesNetApi
   module Models
+
+    # Class to represent School information extracted from Aeries site.
     class School < AeriesNetApi::Models::BaseModel
 #ToDo: make sure that this code executes just once.  Looks like only runs when class is first instantiated
+
+      # List of attributes to be extracted from Aeries site.
       @@aeries_attributes_list= %w{SchoolCode Name InactiveStatusCode Address AddressCity AddressState AddressZipCode AddressZipExt DoNotReport StateCountyID StateDistrictID
            StateSchoolID LowGradeLevel HighGradeLevel Terms}
 
+      # List of methods to be used to create object's attributes dynamically.
       @@setters_list = self.process_aeries_attributes(@@aeries_attributes_list)
 
       puts "executing ****"
       # ToDo: End of code to test for running once.
 
+      # Creates a new object from data received from Aeries site.  It creates a new empty object if no data is received.
       def initialize(aeries_data=nil)
         parse(aeries_data, @@aeries_attributes_list, @@setters_list) if aeries_data.present?
       end
@@ -22,6 +25,7 @@ module AeriesNetApi
         model_inspect(@@setters_list)
       end
 
+      # Returns a hash of attributes extracted from Aeries site
       def attributes
         model_attributes(@@setters_list)
       end
