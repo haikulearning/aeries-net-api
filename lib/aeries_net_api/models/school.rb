1b8e1ef055cdf84292acddd1d7cf32d43a184117
tgarrido-avantica <tomas.garrido@avantica.net>
2015-06-30 13:57:43 -0700

    Adds parsing of terms json array to create array of Term models

--- lib/aeries_net_api/models/school.rb
+++ lib/aeries_net_api/models/school.rb
@@ -3,7 +3,6 @@ module AeriesNetApi
 
     # Class to represent School information extracted from Aeries site.
     class School < AeriesNetApi::Models::BaseModel
-#ToDo: make sure that this code executes just once.  Looks like only runs when class is first instantiated
 
       # List of attributes to be extracted from Aeries site.
       @@aeries_attributes_list= %w{SchoolCode Name InactiveStatusCode Address AddressCity AddressState AddressZipCode AddressZipExt DoNotReport StateCountyID StateDistrictID
@@ -11,9 +10,6 @@ module AeriesNetApi
 
       # List of methods to be used to create object's attributes dynamically.
       @@setters_list = self.process_aeries_attributes(@@aeries_attributes_list)
-
-      puts "executing ****"
-      # ToDo: End of code to test for running once.
 
       # Creates a new object from data received from Aeries site.  It creates a new empty object if no data is received.
       def initialize(aeries_data=nil)
@@ -28,6 +24,16 @@ module AeriesNetApi
       # Returns a hash of attributes extracted from Aeries site
       def attributes
         model_attributes(@@setters_list)
+      end
+
+      # Overrides parse to create array of terms
+      def parse(aeries_data, aeries_attributes_list, setters_list)
+        super
+        terms_list=[]
+        self.terms.each do |term|
+          terms_list << AeriesNetApi::Models::Term.new(term)
+        end
+        self.terms=terms_list
       end
 
     end
