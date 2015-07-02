9c814f8e5966cd397e7c43627ed2bf53efb29870
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-02 15:15:50 -0600

    Documents AeriesNetApi::Support module

--- lib/aeries_net_api/support.rb
+++ lib/aeries_net_api/support.rb
@@ -1,13 +1,17 @@
-# ToDo: Create documentation
+# Adds useful methods to Object class
 module AeriesNetApi
   module Support
+
+    # An object is blank if it’s false, empty, or a whitespace string. For example, ”, ‘ ’, nil, [], and {} are all blank.
     def blank?
       return true if nil?
       return self.gsub(/\s/, '').length==0 if self.kind_of? String
       return self.empty? if self.respond_to? :empty?
+      return true if self.kind_of? FalseClass
       false
     end
 
+    # An object is present if it’s not blank.
     def present?
       !blank?
     end
