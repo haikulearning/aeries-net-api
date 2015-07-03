966b03df1fb2f41b93a363d149fc740ebc70dc17
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-03 15:09:23 -0600

    Improves testing of Support methods

--- spec/aeries_net_api_spec.rb
+++ spec/aeries_net_api_spec.rb
@@ -16,6 +16,14 @@ describe AeriesNetApi do
       expect('   '.blank?).to be true
     end
 
+    it 'should return true for an empty string' do
+      expect(''.blank?).to be true
+    end
+
+    it 'should return false for a valid string' do
+      expect('abc'.blank?).to be false
+    end
+
     it 'should return true for a nil object' do
       expect(nil.blank?).to be true
     end
@@ -24,15 +32,23 @@ describe AeriesNetApi do
       expect([].blank?).to be true
     end
 
+    it 'should return false for a non empty array' do
+      expect([1,2,3].blank?).to be false
+    end
+
     it 'should return true for an empty hash' do
       expect({}.blank?).to be true
+    end
+
+    it 'should return false for a non empty hash' do
+      expect({:a => 1, :b => 2}.blank?).to be false
     end
 
     it 'should return true for a boolean false' do
       expect(false.blank?).to be true
     end
 
-    it 'should return false for a booean true' do
+    it 'should return false for a boolean true' do
       expect(true.blank?).to be false
     end
 
