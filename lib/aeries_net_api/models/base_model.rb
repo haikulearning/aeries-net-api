09cea1243748d64f681ef4450605c40c09cf8cbe
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-08 16:41:06 -0600

    Improves documentation for BaseModel

--- lib/aeries_net_api/models/base_model.rb
+++ lib/aeries_net_api/models/base_model.rb
@@ -1,6 +1,3 @@
-# ToDo:  create documentation
-# ToDo: Create tests for underscore and parse methods.
-
 module AeriesNetApi
   module Models
     class BaseModel
@@ -13,6 +10,10 @@ module AeriesNetApi
         end
       end
 
+      # Dynamically creates attributes for class
+      # Parameters
+      # aeries_attributes - List of Aeries attributes to be stored.
+      # Returns list of methods to be invoked when parseing object
       def self.process_aeries_attributes(aeries_attributes)
         return_list=aeries_attributes.map { |attribute| "#{self.underscore(attribute)}=".to_sym }
         return_list.each do |attribute|
@@ -24,7 +25,11 @@ module AeriesNetApi
 
       protected
 
-      # Returns hash with attributes and values
+      # Creates hash with attributes and values
+      # Parameters
+      # setters_list  - List of methods used to create/update attributes
+      # Return
+      # Hash of attributes with their values
       def model_attributes(setters_list)
         attributes={}
         setters_list.each do |setter|
@@ -36,6 +41,10 @@ module AeriesNetApi
       end
 
       # Overrides 'inspect' method to show only dynamically created attributes corresponding to Aeries attributes
+      # Parameters
+      # setters_list  - list of methods used to create/update attributes
+      # Return
+      # String with similar content to Object#inspect
       def model_inspect(setters_list)
         attributes_list=self.model_attributes(setters_list)
         "#<#{self.class}:0x#{sprintf('%014x',self.object_id<<1)} #{attributes_list.inspect}>"
@@ -43,6 +52,10 @@ module AeriesNetApi
 
       # Convert Camel case words into underscore separated words.
       # Example: underscore 'SchoolName'  =>  school_name
+      # Parameters
+      # camel_case_word - word to be converted
+      # Return
+      # Word converted to snake case
       def self.underscore(camel_case_word)
         camel_case_word.gsub(/::/, '/').
             gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
