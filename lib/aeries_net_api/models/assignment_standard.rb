e3932ac17c1cc38937eaad1e9ba0da46e1801f9c
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-09 15:55:04 -0600

    Fixes rubocop offenses

--- lib/aeries_net_api/models/assignment_standard.rb
+++ lib/aeries_net_api/models/assignment_standard.rb
@@ -1,18 +1,16 @@
 module AeriesNetApi
   module Models
-
     # Class to represent AssignmentStandard information extracted from Aeries site.
     class AssignmentStandard < AeriesNetApi::Models::BaseModel
-
       # List of attributes to be extracted from Aeries site.
-      @@aeries_attributes_list= %w{AcademicBenchmarkId AssignmentNumber GradebookNumber NumberCorrectPossible
-        StandardDescription StandardId}
+      @@aeries_attributes_list = %w(AcademicBenchmarkId AssignmentNumber GradebookNumber NumberCorrectPossible
+                                    StandardDescription StandardId)
 
       # List of methods to be used to create object's attributes dynamically.
-      @@setters_list = self.process_aeries_attributes(@@aeries_attributes_list)
+      @@setters_list = process_aeries_attributes(@@aeries_attributes_list)
 
       # Creates a new object from data received from Aeries site.  It creates a new empty object if no data is received.
-      def initialize(aeries_data=nil)
+      def initialize(aeries_data = nil)
         parse(aeries_data, @@aeries_attributes_list, @@setters_list) if aeries_data.present?
       end
 
@@ -25,7 +23,6 @@ module AeriesNetApi
       def attributes
         model_attributes(@@setters_list)
       end
-
     end
   end
 end
