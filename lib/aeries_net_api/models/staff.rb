9ecaafe380909b9558f0989e5e9de7e5c28b017b
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-09 17:02:41 -0600

    Fixes rubocop offenses

--- lib/aeries_net_api/models/staff.rb
+++ lib/aeries_net_api/models/staff.rb
@@ -1,18 +1,17 @@
 module AeriesNetApi
   module Models
-
     # Class to represent Staff information extracted from Aeries site.
     class Staff < AeriesNetApi::Models::BaseModel
-
       # List of attributes to be extracted from Aeries site.
-      @@aeries_attributes_list= %w{ID FirstName LastName MiddleName Sex BirthYear BirthDate FullTimePercentage HireDate
-        LeaveDate InactiveStatusCode StateEducatorID UserName EmailAddress PrimaryAeriesSchool}
+      @@aeries_attributes_list = %w(ID FirstName LastName MiddleName Sex BirthYear BirthDate FullTimePercentage HireDate
+                                    LeaveDate InactiveStatusCode StateEducatorID UserName EmailAddress
+                                    PrimaryAeriesSchool)
 
       # List of methods to be used to create object's attributes dynamically.
-      @@setters_list = self.process_aeries_attributes(@@aeries_attributes_list)
+      @@setters_list = process_aeries_attributes(@@aeries_attributes_list)
 
       # Creates a new object from data received from Aeries site.  It creates a new empty object if no data is received.
-      def initialize(aeries_data=nil)
+      def initialize(aeries_data = nil)
         parse(aeries_data, @@aeries_attributes_list, @@setters_list) if aeries_data.present?
       end
 
@@ -29,11 +28,10 @@ module AeriesNetApi
       # Overrides 'parse' method to parse dates
       def parse(aeries_data, aeries_attributes_list, setters_list)
         super
-        self.birth_date=DateTime.parse(self.birth_date) unless self.birth_date.nil?
-        self.hire_date=DateTime.parse(self.hire_date) unless self.hire_date.nil?
-        self.leave_date=DateTime.parse(self.leave_date) unless self.leave_date.nil?
+        self.birth_date = DateTime.parse(birth_date) unless birth_date.nil?
+        self.hire_date = DateTime.parse(hire_date) unless hire_date.nil?
+        self.leave_date = DateTime.parse(leave_date) unless leave_date.nil?
       end
-
     end
   end
 end
