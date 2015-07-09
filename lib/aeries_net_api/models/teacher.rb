d10dfbdf1462aa66d8a8e5ef2643d99b08f10ede
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-09 17:10:46 -0600

    Fixes rubocop offenses

--- lib/aeries_net_api/models/teacher.rb
+++ lib/aeries_net_api/models/teacher.rb
@@ -1,19 +1,17 @@
 module AeriesNetApi
   module Models
-
     # Class to represent Teacher information extracted from Aeries site.
     class Teacher < AeriesNetApi::Models::BaseModel
-
       # List of attributes to be extracted from Aeries site.
-      @@aeries_attributes_list= %w{SchoolCode TeacherNumber DisplayName FirstName LastName Room EmailAddress StaffID1
-        StaffID2 StaffID3 StateCourseCode InactiveStatusCode HighlyQualifiedStatusCode1 HighlyQualifiedStatusCode2
-        HighlyQualifiedStatusCode3}
+      @@aeries_attributes_list = %w(SchoolCode TeacherNumber DisplayName FirstName LastName Room EmailAddress StaffID1
+                                    StaffID2 StaffID3 StateCourseCode InactiveStatusCode HighlyQualifiedStatusCode1
+                                    HighlyQualifiedStatusCode2 HighlyQualifiedStatusCode3)
 
       # List of methods to be used to create object's attributes dynamically.
-      @@setters_list = self.process_aeries_attributes(@@aeries_attributes_list)
+      @@setters_list = process_aeries_attributes(@@aeries_attributes_list)
 
       # Creates a new object from data received from Aeries site.  It creates a new empty object if no data is received.
-      def initialize(aeries_data=nil)
+      def initialize(aeries_data = nil)
         parse(aeries_data, @@aeries_attributes_list, @@setters_list) if aeries_data.present?
       end
 
@@ -26,7 +24,6 @@ module AeriesNetApi
       def attributes
         model_attributes(@@setters_list)
       end
-
     end
   end
 end
