460df389bfb66f703b9cb11428a794932230c5b5
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-09 16:59:18 -0600

    Fixes rubocop offenses

--- lib/aeries_net_api/models/gradebook_term.rb
+++ lib/aeries_net_api/models/gradebook_term.rb
@@ -1,17 +1,15 @@
 module AeriesNetApi
   module Models
-
     # Class to represent GradebokTerm information extracted from Aeries site.
     class GradebookTerm < AeriesNetApi::Models::BaseModel
-
       # List of attributes to be extracted from Aeries site.
-      @@aeries_attributes_list= %w{GradebookNumber Code Name StartDate EndDate}
+      @@aeries_attributes_list = %w(GradebookNumber Code Name StartDate EndDate)
 
       # List of methods to be used to create object's attributes dynamically.
-      @@setters_list = self.process_aeries_attributes(@@aeries_attributes_list)
+      @@setters_list = process_aeries_attributes(@@aeries_attributes_list)
 
       # Creates a new object from data received from Aeries site.  It creates a new empty object if no data is received.
-      def initialize(aeries_data=nil)
+      def initialize(aeries_data = nil)
         parse(aeries_data, @@aeries_attributes_list, @@setters_list) if aeries_data.present?
       end
 
@@ -28,10 +26,9 @@ module AeriesNetApi
       # Overrides 'parse' method to parse dates
       def parse(aeries_data, aeries_attributes_list, setters_list)
         super
-        self.start_date=DateTime.parse(self.start_date) unless self.start_date.nil?
-        self.end_date=DateTime.parse(self.end_date) unless self.end_date.nil?
+        self.start_date = DateTime.parse(start_date) unless start_date.nil?
+        self.end_date = DateTime.parse(end_date) unless end_date.nil?
       end
-
     end
   end
 end
