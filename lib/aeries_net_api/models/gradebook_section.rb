c254a2edb547069e22e97d267e1590608309cd16
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-09 16:49:33 -0600

    Fixes rubocop offenses

--- lib/aeries_net_api/models/gradebook_section.rb
+++ lib/aeries_net_api/models/gradebook_section.rb
@@ -1,17 +1,15 @@
 module AeriesNetApi
   module Models
-
     # Class to represent GradebookSection information extracted from Aeries site.
     class GradebookSection < AeriesNetApi::Models::BaseModel
-
       # List of attributes to be extracted from Aeries site.
-      @@aeries_attributes_list= %w{GradebookNumber SectionNumber SchoolCode}
+      @@aeries_attributes_list = %w(GradebookNumber SectionNumber SchoolCode)
 
       # List of methods to be used to create object's attributes dynamically.
-      @@setters_list = self.process_aeries_attributes(@@aeries_attributes_list)
+      @@setters_list = process_aeries_attributes(@@aeries_attributes_list)
 
       # Creates a new object from data received from Aeries site.  It creates a new empty object if no data is received.
-      def initialize(aeries_data=nil)
+      def initialize(aeries_data = nil)
         parse(aeries_data, @@aeries_attributes_list, @@setters_list) if aeries_data.present?
       end
 
@@ -24,7 +22,6 @@ module AeriesNetApi
       def attributes
         model_attributes(@@setters_list)
       end
-
     end
   end
 end
