6fc236dba1a3335245b58cb1ddee0a762f8427c5
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-09 17:07:48 -0600

    Fixes rubocop offenses

--- lib/aeries_net_api/models/student_class.rb
+++ lib/aeries_net_api/models/student_class.rb
@@ -1,18 +1,16 @@
 module AeriesNetApi
   module Models
-
     # Class to represent StudentClass information extracted from Aeries site.
     class StudentClass < AeriesNetApi::Models::BaseModel
-
       # List of attributes to be extracted from Aeries site.
-      @@aeries_attributes_list= %w{PermanentID SchoolCode SequenceNumber CourseID SectionNumber Period PeriodBlock
-        TeacherNumber DateStarted DateEnded}
+      @@aeries_attributes_list = %w(PermanentID SchoolCode SequenceNumber CourseID SectionNumber Period PeriodBlock
+                                    TeacherNumber DateStarted DateEnded)
 
       # List of methods to be used to create object's attributes dynamically.
-      @@setters_list = self.process_aeries_attributes(@@aeries_attributes_list)
+      @@setters_list = process_aeries_attributes(@@aeries_attributes_list)
 
       # Creates a new object from data received from Aeries site.  It creates a new empty object if no data is received.
-      def initialize(aeries_data=nil)
+      def initialize(aeries_data = nil)
         parse(aeries_data, @@aeries_attributes_list, @@setters_list) if aeries_data.present?
       end
 
@@ -29,10 +27,9 @@ module AeriesNetApi
       # Overrides 'parse' method to parse dates
       def parse(aeries_data, aeries_attributes_list, setters_list)
         super
-        self.date_started=DateTime.parse(self.date_started) unless self.date_started.nil?
-        self.date_ended=DateTime.parse(self.date_ended) unless self.date_ended.nil?
+        self.date_started = DateTime.parse(date_started) unless date_started.nil?
+        self.date_ended = DateTime.parse(date_ended) unless date_ended.nil?
       end
-
     end
   end
 end
