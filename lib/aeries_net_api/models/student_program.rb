d68b3c6cf455b6613bf6b7e167c51a6cefaa8a0b
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-09 17:10:28 -0600

    Fixes rubocop offenses

--- lib/aeries_net_api/models/student_program.rb
+++ lib/aeries_net_api/models/student_program.rb
@@ -1,18 +1,16 @@
 module AeriesNetApi
   module Models
-
     # Class to represent Staff information extracted from Aeries site.
     class StudentProgram < AeriesNetApi::Models::BaseModel
-
       # List of attributes to be extracted from Aeries site.
-      @@aeries_attributes_list= %w{StudentID ProgramCode ProgramDescription EligibilityStartDate EligibilityEndDate
-        ParticipationStartDate ParticipationEndDate}
+      @@aeries_attributes_list = %w(StudentID ProgramCode ProgramDescription EligibilityStartDate EligibilityEndDate
+                                    ParticipationStartDate ParticipationEndDate)
 
       # List of methods to be used to create object's attributes dynamically.
-      @@setters_list = self.process_aeries_attributes(@@aeries_attributes_list)
+      @@setters_list = process_aeries_attributes(@@aeries_attributes_list)
 
       # Creates a new object from data received from Aeries site.  It creates a new empty object if no data is received.
-      def initialize(aeries_data=nil)
+      def initialize(aeries_data =  nil)
         parse(aeries_data, @@aeries_attributes_list, @@setters_list) if aeries_data.present?
       end
 
@@ -29,10 +27,10 @@ module AeriesNetApi
       # Overrides 'parse' method to parse dates
       def parse(aeries_data, aeries_attributes_list, setters_list)
         super
-        self.eligibility_start_date=DateTime.parse(self.eligibility_start_date) unless self.eligibility_start_date.nil?
-        self.eligibility_end_date=DateTime.parse(self.eligibility_end_date) unless self.eligibility_end_date.nil?
-        self.participation_start_date=DateTime.parse(self.participation_start_date) unless self.participation_start_date.nil?
-        self.participation_end_date=DateTime.parse(self.participation_end_date) unless self.participation_end_date.nil?
+        self.eligibility_start_date = DateTime.parse(eligibility_start_date) unless eligibility_start_date.nil?
+        self.eligibility_end_date = DateTime.parse(eligibility_end_date) unless eligibility_end_date.nil?
+        self.participation_start_date = DateTime.parse(participation_start_date) unless participation_start_date.nil?
+        self.participation_end_date = DateTime.parse(participation_end_date) unless participation_end_date.nil?
       end
     end
   end
