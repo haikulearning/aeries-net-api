79b0a0cc3d6a2ce474ed4dd14562bf4557a33356
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-10 11:43:44 -0600

    Fixes rubocop offenses

--- spec/models/final_mark_spec.rb
+++ spec/models/final_mark_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe AeriesNetApi::Models::FinalMark do
-
- let(:test_class) {AeriesNetApi::Models::FinalMark}
+  let(:test_class) { AeriesNetApi::Models::FinalMark }
 
   context 'methods' do
     it 'should create an empty instance of AeriesNetApi::Models::FinalMark' do
@@ -10,12 +9,12 @@ describe AeriesNetApi::Models::FinalMark do
     end
 
     it 'attributes should return  a list of Aeries attributes' do
-      object=test_class.new
+      object = test_class.new
       expect(object.attributes).to be_instance_of Hash
     end
 
     it 'inspect should return a string describing object' do
-      object=test_class.new
+      object = test_class.new
       expect(object.inspect).to be_instance_of String
       expect(object.inspect).to be_include test_class.name
     end
