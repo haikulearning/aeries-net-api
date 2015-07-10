b591e9f194b3d0719223fd0f0f95ea69d926a885
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-10 10:20:04 -0600

    Fixes rubocop offenses

--- lib/aeries_net_api/support.rb
+++ lib/aeries_net_api/support.rb
@@ -1,17 +1,16 @@
-# Adds useful methods to Object class
 module AeriesNetApi
+  # Adds useful methods to Object class
   module Support
-
-    # An object is blank if it’s false, empty, or a whitespace string. For example, ”, ‘ ’, nil, [], and {} are all blank.
+    # An object is blank if it's false, empty, or a whitespace string. For example, " ", ' '', nil, [], and {} are all blank.
     def blank?
       return true if nil?
-      return self.gsub(/\s/, '').length==0 if self.kind_of? String
-      return self.empty? if self.respond_to? :empty?
-      return true if self.kind_of? FalseClass
+      return gsub(/\s/, '').length == 0 if is_a? String
+      return empty? if respond_to? :empty?
+      return true if self.is_a? FalseClass
       false
     end
 
-    # An object is present if it’s not blank.
+    # An object is present if it's not blank.
     def present?
       !blank?
     end
