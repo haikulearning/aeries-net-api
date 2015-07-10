57ced2cc29a83eb1430086984e4e17f845015566
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-10 11:41:34 -0600

    Fixes rubocop offenses

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe AeriesNetApi::Models::Course do
-
- let(:test_class) {AeriesNetApi::Models::Course}
+  let(:test_class) { AeriesNetApi::Models::Course }
 
   context 'methods' do
     it 'should create an empty instance of AeriesNetApi::Models::Course' do
@@ -10,12 +9,12 @@ describe AeriesNetApi::Models::Course do
     end
 
     it 'attributes should  return  a list of Aeries attributes' do
-      object=test_class.new
+      object = test_class.new
       expect(object.attributes).to be_instance_of Hash
     end
 
     it 'inspect should return a string describing object' do
-      object=test_class.new
+      object = test_class.new
       expect(object.inspect).to be_instance_of String
       expect(object.inspect).to be_include test_class.name
     end
