3d61b5bd1c6e777488088382dea4a3b33ee13a20
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-10 11:52:42 -0600

    Fixes rubocop offenses

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -1,20 +1,18 @@
 require 'spec_helper'
 
 describe AeriesNetApi::Models::School do
-
   context 'methods' do
-
     it 'should create an empty instance of AeriesNetApi::Models::School' do
       expect(AeriesNetApi::Models::School.new).to be_instance_of AeriesNetApi::Models::School
     end
 
     it 'attributes should  return  a list of Aeries attributes' do
-      school=AeriesNetApi::Models::School.new
+      school = AeriesNetApi::Models::School.new
       expect(school.attributes).to be_instance_of Hash
     end
 
     it 'inspect should return a string describing object' do
-      school=AeriesNetApi::Models::School.new
+      school = AeriesNetApi::Models::School.new
       expect(school.inspect).to be_instance_of String
       expect(school.inspect).to be_include 'AeriesNetApi::Models::School'
     end
