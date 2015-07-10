45772c3fa6d154778d6bd26385c67084ec342ceb
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-10 10:42:04 -0600

    Fixes rubocop offenses

--- spec/models/assignment_standard_spec.rb
+++ spec/models/assignment_standard_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe AeriesNetApi::Models::AssignmentStandard do
-
- let(:test_class) {AeriesNetApi::Models::AssignmentStandard}
+  let(:test_class) { AeriesNetApi::Models::AssignmentStandard }
 
   context 'methods' do
     it 'should create an empty instance of AeriesNetApi::Models::AssignmentStandard' do
@@ -10,12 +9,12 @@ describe AeriesNetApi::Models::AssignmentStandard do
     end
 
     it 'attributes should  return  a list of Aeries attributes' do
-      object=test_class.new
+      object = test_class.new
       expect(object.attributes).to be_instance_of Hash
     end
 
     it 'inspect should return a string describing object' do
-      object=test_class.new
+      object = test_class.new
       expect(object.inspect).to be_instance_of String
       expect(object.inspect).to be_include test_class.name
     end
