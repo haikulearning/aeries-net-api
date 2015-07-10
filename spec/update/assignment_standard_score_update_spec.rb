37275f486ca66a4004fec22756bdb3eb5b70d1d4
tgarrido-avantica <tomas.garrido@avantica.net>
2015-07-10 11:59:39 -0600

    Fixes rubocop offenses

--- spec/update/assignment_standard_score_update_spec.rb
+++ spec/update/assignment_standard_score_update_spec.rb
@@ -4,8 +4,8 @@ describe AeriesNetApi::Update::AssignmentStandardScoreUpdate do
   describe 'constructor'
   context 'right parameters' do
     it 'should set object properties' do
-      update_object = AeriesNetApi::Update::AssignmentStandardScoreUpdate.new(:aeries_standard_id => 123,
-        :academic_benchmark_id=>5,:number_correct=>4.25)
+      update_object = AeriesNetApi::Update::AssignmentStandardScoreUpdate.new(:aeries_standard_id    => 123,
+                                                                              :academic_benchmark_id => 5, :number_correct => 4.25)
       expect(update_object.aeries_standard_id).to eql 123
       expect(update_object.academic_benchmark_id).to eql 5
       expect(update_object.number_correct).to eql 4.25
@@ -15,9 +15,8 @@ describe AeriesNetApi::Update::AssignmentStandardScoreUpdate do
   context 'wrong parameters' do
     it 'should raise an error for invalid parameters' do
       # message should not contain a valid parameter like permanent_id
-      expect { AeriesNetApi::Update::AssignmentStandardScoreUpdate.new(:wrong => 'my_site', :another_wrong => 'abc',:permanent_id=>'123') }.to \
-          raise_error(ArgumentError) {|error| expect(!(error.message=~/wrong/).nil? && (error.message =~ /aeries_standard_id/).nil?).to be true  }
+      expect { AeriesNetApi::Update::AssignmentStandardScoreUpdate.new(:wrong => 'my_site', :another_wrong => 'abc', :permanent_id => '123') }.to \
+        raise_error(ArgumentError) { |error| expect(!(error.message =~ /wrong/).nil? && (error.message =~ /aeries_standard_id/).nil?).to be true }
     end
   end
-
 end
